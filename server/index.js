352829f06f1e5cef7465eef6cb42383a41793130
KishwarEdgeforce <kishwari@edgeforce.in>
2023-04-15 13:16:45 +0530

    first commit

--- server/index.js
+++ server/index.js
@@ -130,6 +130,6 @@ app.get("/", (req, res) => {
 
 
 
-app.listen(5000, () => {
-    console.log("Server is running on port 5000");
+app.listen(5001, () => {
+    console.log("Server is running on port 5001");
 })
